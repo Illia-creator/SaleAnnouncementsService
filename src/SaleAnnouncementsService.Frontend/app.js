5c7a88db64effd29387985934ab252f62e05e861
Illia-creator <sie29092002@gmail.com>
2023-09-09 23:31:46 +0300

    #4 Added Frontend Basic

---
+++ src/SaleAnnouncementsService.Frontend/app.js
@@ -0,0 +1,28 @@
+﻿const App = {
+    data() {
+        return {
+            counter: 0,
+            title: "Counter",
+            placeholderAttribute: "Enter Topic",
+            inputTopic: "",
+            notes: ["first"]
+
+        }
+    },
+
+    methods:{
+        inputValueHandler(event){
+            this.inputTopic = event.target.value            
+        },
+        addNewNote(){
+            this.notes.push(this.inputTopic)
+            this.inputTopic = ""
+        },
+        deleteNote(idx){
+            this.notes.splice(idx, 1)
+            }
+    }
+}
+
+
+Vue.createApp(App).mount('#app-div')
